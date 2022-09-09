dd7fa18281818a230a0fdafff814c0f7e9b94ef6
dilaksha1 <110473812+dilaksha1@users.noreply.github.com>
2022-09-09 11:15:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['+94774473814']
+global.premium = ['+94774473814']
+global.ownernomer = '+94774473814'
+global.ownername = 'DILAKSHA'
+global.botname = 'HASHI-MD'
+global.footer = '©HashiMDInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'https://www.youtube.com/channel/UCT0n0uBX_4b-YSAYyj5sAGQ'
+global.packname = '❤✔HASHI-MD❤✔'
+global.author = 'Dilaksha'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
